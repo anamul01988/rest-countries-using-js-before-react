5987bd22547703377a7391ad977cec9d00d95bd3
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-16 20:09:08 +0600

    first commit

--- js/coutries.js
+++ js/coutries.js
@@ -5,9 +5,9 @@ const loadCountries = () =>{
 }
 
 const displayCountries = countries =>{
-    // console.log(countries[0]);
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
-    // console.log(countriesHTML[0]);
+    console.log(countriesHTML[0]);
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
